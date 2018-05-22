7e075d5e80933804a2f1013fbc69f27cb42d6411
Stuart Leeks <stuartle@microsoft.com>
2018-05-22 09:23:05 +0100

    Rename functions

--- azext_show-deployments/__init__.py
+++ azext_show-deployments/__init__.py
@@ -12,24 +12,24 @@ helps['group deployment watch'] = """
     short-summary: Say hello world.
 """
 
-def helloworld():
+def watchDeployment():
     print('Hello World!!')
 
 
-class HelloWorldCommandsLoader(AzCommandsLoader):
+class WatchDeploymentCommandsLoader(AzCommandsLoader):
 
     def __init__(self, cli_ctx=None):
         from azure.cli.core.commands import CliCommandType
         custom_type = CliCommandType(operations_tmpl='azext_show-deployments#{}')
-        super(HelloWorldCommandsLoader, self).__init__(cli_ctx=cli_ctx,
+        super(WatchDeploymentCommandsLoader, self).__init__(cli_ctx=cli_ctx,
                                                        custom_command_type=custom_type)
 
     def load_command_table(self, args):
         with self.command_group('group deployment') as g:
-            g.custom_command('watch', 'helloworld')
+            g.custom_command('watch', 'watchDeployment')
         return self.command_table
 
     def load_arguments(self, _):
         pass
 
-COMMAND_LOADER_CLS = HelloWorldCommandsLoader
+COMMAND_LOADER_CLS = WatchDeploymentCommandsLoader
