607090d61b1284c8cc8643b26c5509462a5b277d
Stuart Leeks <stuartle@microsoft.com>
2018-05-22 13:08:16 +0100

    Look up deployment, Add TODOs

---
+++ azext_show-deployments/cli_utils.py
@@ -0,0 +1,56 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import sys
+import json
+
+from subprocess import check_output, STDOUT, CalledProcessError
+from knack.util import CLIError
+
+from knack.log import get_logger
+logger = get_logger(__name__)
+
+EXTENSION_TAG_STRING = 'created_by=image-copy-extension'
+
+
+# pylint: disable=inconsistent-return-statements
+def run_cli_command(cmd, return_as_json=False):
+    try:
+        cmd_output = check_output(cmd, stderr=STDOUT, universal_newlines=True)
+        logger.debug('command: %s ended with output: %s', cmd, cmd_output)
+
+        if return_as_json:
+            if cmd_output:
+                json_output = json.loads(cmd_output)
+                return json_output
+            else:
+                raise CLIError("Command returned an unexpected empty string.")
+        else:
+            return cmd_output
+    except CalledProcessError as ex:
+        logger.error('command failed: %s', cmd)
+        logger.error('output: %s', ex.output)
+        raise ex
+    except:
+        logger.error('command ended with an error: %s', cmd)
+        raise
+
+
+def prepare_cli_command(cmd, output_as_json=True, tags=None):
+    full_cmd = [sys.executable, '-m', 'azure.cli'] + cmd
+
+    if output_as_json:
+        full_cmd += ['--output', 'json']
+    else:
+        full_cmd += ['--output', 'tsv']
+
+    # tag newly created resources, containers don't have tags
+    if 'create' in cmd and ('container' not in cmd):
+        full_cmd += ['--tags', EXTENSION_TAG_STRING]
+
+        if tags is not None:
+            full_cmd += tags.split()
+
+    return full_cmd
--- azext_show-deployments/watch_deployments.py
+++ azext_show-deployments/watch_deployments.py
@@ -2,22 +2,32 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
- 
+import json
 from knack.help_files import helps
-
 from azure.cli.core.commands import CliCommandType
 from azure.cli.core import AzCommandsLoader
+from .cli_utils import run_cli_command, prepare_cli_command
+
+
+
 
 helps['group deployment watch'] = """
     type: command
-    short-summary: Say hello world.
+    short-summary: Watch an ARM deployment
+    parameters:
+        - name: --resource-group -g
+        type: string
+        short-summary: The name of the resource group
+        - name: --deployment -n
+        type: string
+        short-summary: The name of the deployment to watch
 """
 
 def watch_deployment(resourcegroupname, deploymentname): #resource_group_name, deployment_name):
-    print('Hello World!!')
-    print(resourcegroupname)
-    print(deploymentname)
-    print('TODO - implement!')
+    cli_cmd = prepare_cli_command(['group', 'deployment', 'show', '-g', resourcegroupname, '-n', deploymentname])
+    deploymentjson = run_cli_command(cli_cmd)
+    deployment = json.loads(deploymentjson)
+    return deployment['id']
 
 
 def load_command_table(self, args):
@@ -32,3 +42,14 @@ def load_arguments(self, _):
         c.argument('resourcegroupname', options_list=['--resource-group', '-g']) # TODO - how to add completion?
 
 
+# TODO
+# Check deployment status
+# Write deployment name, status, duration (if running!)
+# Get operations
+# List operations
+# Get child deployments
+# refresh
+# colour code output
+# dump outputs when complete
+# default to latest deployment if not specified
+# allow refresh interval to be specified
