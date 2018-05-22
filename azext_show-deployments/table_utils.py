f2f51e12808bcf5a64b51979897843b888a184ee
Stuart Leeks <stuartle@microsoft.com>
2018-05-22 21:26:25 +0100

    Add coloured output

--- azext_show-deployments/table_utils.py
+++ azext_show-deployments/table_utils.py
@@ -1,24 +1,33 @@
 
 class Table:
-    def __init__ (self, headers, rows):
+    def __init__ (self, headers, rows, use_last_column_for_color=False):
         self.headers = headers
         self.rows = rows
+        self.use_last_column_for_color = use_last_column_for_color
+
+        self.column_count = len(self.headers)
+        if (use_last_column_for_color):
+            self.column_count = self.column_count - 1
     
     def print_table(self):
         # calculate the column widths
         widths = [len(h) for h in self.headers]
         for row in self.rows:
-            for i in range(5):
+            for i in range(self.column_count):
                 widths[i] = max(widths[i], len(str(row[i])))
 
-        print(self.build_row(self.headers, widths))
+        print(self.build_row(self.headers + [''], widths)) # add extra item to headers in case using last column for colors!
 
         for row in self.rows:
             print(self.build_row(row, widths))
         
     def build_row(self, row, widths):
-        output = ""
-        for i in range(len(row)):
+        if (self.use_last_column_for_color):
+            output = row[-1]
+        else:
+            output = ""
+
+        for i in range(self.column_count):
             output += self.pad_and_trunc(row[i], widths[i]) + " "
         return output
 
--- azext_show-deployments/watch_deployments.py
+++ azext_show-deployments/watch_deployments.py
@@ -25,6 +25,16 @@ helps['group deployment watch'] = """
         short-summary: The name of the deployment to watch
 """
 
+def color_for_state(state):
+    if state == 'Succeeded':
+        return '\033[1;30m' # bright black
+    if state == 'Failed':
+        return '\033[31m' # red
+    if state == 'Running':
+        return '\033[32m' # green
+    return "\033[0m" # reset
+
+
 def watch_deployment(resourcegroupname, deploymentname):
     deployment = cli_as_json(['group', 'deployment', 'show', '-g', resourcegroupname, '-n', deploymentname])
     
@@ -42,15 +52,13 @@ def watch_deployment(resourcegroupname, deploymentname):
     print()
 
     cli_operations = cli_as_json(['group', 'deployment', 'operation', 'list', '-g', resourcegroupname, '-n', deploymentname])
-    operations = map(Operation, cli_operations) 
-    operations = sorted(operations, key = lambda o: o.timestamp)
-
+    operations = sorted(map(Operation, cli_operations) , key = lambda o: o.timestamp)
 
     headers = ['State', 'ResourceType', 'ResourceName', 'StartTime', 'Duration']
-    operation_rows = map(lambda o: [o.provisioning_state, o.resource_type, o.resource_name, o.timestamp, o.duration], operations)
+    operation_rows = map(lambda o: [o.provisioning_state, o.resource_type, o.resource_name, o.timestamp, o.duration, color_for_state(o.provisioning_state)], operations)
     operation_rows = [o for o in operation_rows]
 
-    table = Table(headers, operation_rows)
+    table = Table(headers, operation_rows, use_last_column_for_color=True)
     table.print_table()
 
 
@@ -71,7 +79,6 @@ def load_arguments(self, _):
 # List operations
 # Get child deployments
 # refresh
-# colour code output
 # dump outputs when complete
 # default to latest deployment if not specified
 # allow refresh interval to be specified
